68ba756d55b19552d6771684e0a368c054f05279
Raghu K <raghuk.vit@gmail.com>
2022-06-07 08:27:26 +0530

    Update roboshop-infra/all-modules/tf-module-rabbitmq/

--- data.tf
+++ data.tf
@@ -1,6 +1,5 @@
 data "aws_ami" "ami" {
-  executable_users = ["self"]
-  most_recent      = true
-  name_regex       = "base-with-ansible"
-  owners           = ["self"]
+  most_recent = true
+  name_regex  = "base-with-ansible"
+  owners      = ["self"]
 }
