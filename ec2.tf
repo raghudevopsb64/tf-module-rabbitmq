b6431d8541b0729cbf9fdbe3249e4919663ec226
Raghu K <raghuk.vit@gmail.com>
2022-06-07 08:32:14 +0530

    Update roboshop-infra/all-modules/tf-module-rabbitmq/

--- ec2.tf
+++ ec2.tf
@@ -3,10 +3,16 @@ resource "aws_spot_instance_request" "spot" {
   instance_type          = var.NODE_TYPE
   vpc_security_group_ids = [aws_security_group.main.id]
   subnet_id              = var.SUBNET_IDS[0]
+  wait_for_fulfillment   = true
 
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}"
   }
 }
 
+resource "aws_ec2_tag" "name-tag" {
+  resource_id = aws_spot_instance_request.spot.spot_instance_id
+  key         = "Name"
+  value       = "${var.COMPONENT}-${var.ENV}"
+}
 
